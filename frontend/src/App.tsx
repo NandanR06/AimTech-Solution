88fa6dd67a5e3bb2aed972a0dccfb4f415c948d0
Nandan <nandanr7284@gmail.com>
2025-07-19 22:58:34 +0530

    updating

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route,  Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
